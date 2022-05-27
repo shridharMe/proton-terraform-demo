399b728fcd16f1004b36396d172711eceef15952
Shridhar <36041359+shridharMe@users.noreply.github.com>
2022-05-27 18:55:58 +0100

    Update updateTerraVars.py

--- terraform-operator/updateTerraVars.py
+++ terraform-operator/updateTerraVars.py
@@ -4,9 +4,9 @@ a_file = open("proton.auto.tfvars.json", "r")
 json_object = json.load(a_file)
 a_file.close()
 json_object["environment"]["inputs"]["terraform_cloud_api_token"]  = os.environ['TERRAFROM_CLOUD_API_TOKEN']
-json_object["environment"]["inputs"]["aws_access_key_id"] = os.environ['AWS_ACCESS_KEY_ID']
-json_object["environment"]["inputs"]["aws_secret_access_key"] = os.environ['AWS_SECRET_ACCESS_KEY']
-json_object["environment"]["inputs"]["aws_session_token"] = os.environ['AWS_SESSION_TOKEN']
+json_object["environment"]["inputs"]["aws_access_key_id"] = os.environ['TERRAFROM_AWS_ACCESS_KEY_ID']
+json_object["environment"]["inputs"]["aws_secret_access_key"] = os.environ['TERRAFROM_AWS_SECRET_ACCESS_KEY']
+json_object["environment"]["inputs"]["aws_session_token"] = os.environ['TERRAFROM_AWS_SESSION_TOKEN']
 a_file = open("proton.auto.tfvars.json", "w")
 json.dump(json_object, a_file)
 
