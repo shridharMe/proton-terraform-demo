31caa916f07502e5b28895299761581ab443dfec
Shridhar Patil <saspatil@amazon.co.uk>
2022-05-28 18:38:19 +0100

    added some logic for terraform operator

--- vpc-dev/main.tf
+++ vpc-dev/main.tf
@@ -5,15 +5,17 @@ To manage this resource, see AWS Proton Resource: arn:aws:proton:us-east-2:75369
 
 If the resource is no longer accessible within AWS Proton, it may have been deleted and may require manual cleanup.
 */
-
+locals {
+  local_data = jsondecode(file("${path.module}/proton.auto.tfvars.json"))
+}
 module "vpc" {
   source          = "terraform-aws-modules/vpc/aws"
   version         = "3.13.0"
-  name            = var.environment.inputs.vpc_name
-  cidr            = var.environment.inputs.vpc_cidr
+  name            = local.local_data.environment.inputs.vpc_name
+  cidr            = local.local_data.environment.inputs.vpc_cidr
   azs             = data.aws_availability_zones.available.names
-  private_subnets = [var.environment.inputs.private_subnet_one_cidr,var.environment.inputs.private_subnet_two_cidr,var.environment.inputs.private_subnet_three_cidr ]
-  public_subnets  = [var.environment.inputs.public_subnet_one_cidr,var.environment.inputs.public_subnet_two_cidr,var.environment.inputs.public_subnet_three_cidr ]
+  private_subnets = [local.local_data.environment.inputs.private_subnet_one_cidr,local.local_data.environment.inputs.private_subnet_two_cidr,local.local_data.environment.inputs.private_subnet_three_cidr ]
+  public_subnets  = [local.local_data.environment.inputs.public_subnet_one_cidr,local.local_data.environment.inputs.public_subnet_two_cidr,local.local_data.environment.inputs.public_subnet_three_cidr ]
 
   enable_nat_gateway = true
   enable_vpn_gateway = true
@@ -23,8 +25,8 @@ module "vpc" {
   
   tags = {
     Terraform   = "true"
-    Environment = var.environment.inputs.project_environment
-    Project     = var.environment.inputs.project_name
+    Environment = local.local_data.environment.inputs.project_environment
+    Project     = local.local_data.environment.inputs.project_name
   }
 }
 
@@ -33,7 +35,7 @@ data "aws_availability_zones" "available" {
   state = "available"
   filter {
     name   = "region-name"
-    values = [var.environment.inputs.aws_region]
+    values = [local.local_data.environment.inputs.aws_region]
   }
 }
 
