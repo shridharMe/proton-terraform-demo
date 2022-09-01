85bab49bff9d3b0768276ab486c2e068bb7c40f3
sbx_user1051 <sbx_user1051@169.254.87.29>
2022-09-01 16:50:04 +0000

    Update requested for Proton - service/my-app/service-instance/team1

---
+++ eks-dev/my-app-team1/main.tf
@@ -0,0 +1,13 @@
+/*
+This file is managed by AWS Proton. Any changes made directly to this file will be overwritten the next time AWS Proton performs an update.
+
+To manage this resource, see AWS Proton Resource: arn:aws:proton:us-west-2:753690273280:service/my-app/service-instance/team1
+
+If the resource is no longer accessible within AWS Proton, it may have been deleted and may require manual cleanup.
+*/
+
+resource "kubernetes_namespace" "namespace" {
+  metadata {
+    name = var.service_instance.inputs.namespace_name
+  }
+}
