14bda298a3c165add3617cbe9862c8c9c0e36c90
Shridhar <36041359+shridharMe@users.noreply.github.com>
2022-09-01 18:47:05 +0100

    Delete eks-dev-2 directory

--- eks-dev-2/main.tf
+++
@@ -1,7 +0,0 @@
-/*
-This file is no longer managed by AWS Proton. The associated resource has been deleted in Proton.
-*/
-
-data "aws_eks_cluster" "cluster" {
-  name = var.environment.inputs.cluster_name
-}
