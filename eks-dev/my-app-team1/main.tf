b5f435e48b1a4baa4a0bde057ebb0426a993e993
Shridhar <36041359+shridharMe@users.noreply.github.com>
2022-09-01 18:47:19 +0100

    Delete eks-dev directory

--- eks-dev/my-app-team1/main.tf
+++
@@ -1,9 +0,0 @@
-/*
-This file is no longer managed by AWS Proton. The associated resource has been deleted in Proton.
-*/
-
-resource "kubernetes_namespace" "namespace" {
-  metadata {
-    name = var.service_instance.inputs.namespace_name
-  }
-}
