0be1a5451d3875ea4b2a2b5e6d58b7c6356e5a4a
sbx_user1051 <sbx_user1051@169.254.12.133>
2022-09-02 12:00:20 +0000

    Update requested for Proton - environment/eks-dev

---
+++ eks-dev/versions.tf
@@ -0,0 +1,23 @@
+/*
+This file is no longer managed by AWS Proton. The associated resource has been deleted in Proton.
+*/
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>4.14.0"
+    }
+    template = {
+      source  = "hashicorp/template"
+      version = "~>2.2.0"
+    }
+  }
+  required_version = "~>1.0"
+}
+
+provider "aws" {
+  region  = var.environment.inputs.aws_region
+}
+
+ 
