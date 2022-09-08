91953cf62d3498d93ed896d5679ba3d47fc134bc
sbx_user1051 <sbx_user1051@169.254.126.141>
2022-09-08 15:56:19 +0000

    Update requested for Proton - service/shine-service/service-instance/shine-service-instance

---
+++ vpc-testing/shine-service-shine-service-instance/main.tf
@@ -0,0 +1,13 @@
+/*
+This file is managed by AWS Proton. Any changes made directly to this file will be overwritten the next time AWS Proton performs an update.
+
+To manage this resource, see AWS Proton Resource: arn:aws:proton:us-east-2:753690273280:service/shine-service/service-instance/shine-service-instance
+
+If the resource is no longer accessible within AWS Proton, it may have been deleted and may require manual cleanup.
+*/
+
+resource "kubernetes_namespace" "namwspace" {
+  metadata {
+    name = var.service.inputs.namespace_name
+  }
+}
