8f8893bd7d3ac2fd913e675b0c3327a7e93b9845
sbx_user1051 <sbx_user1051@169.254.70.45>
2022-09-09 13:00:45 +0000

    Update requested for Proton - environment/vpc-testing-final

---
+++ vpc-testing-final/outputs.tf
@@ -0,0 +1,22 @@
+/*
+This file is managed by AWS Proton. Any changes made directly to this file will be overwritten the next time AWS Proton performs an update.
+
+To manage this resource, see AWS Proton Resource: arn:aws:proton:us-east-2:753690273280:environment/vpc-testing-final
+
+If the resource is no longer accessible within AWS Proton, it may have been deleted and may require manual cleanup.
+*/
+
+output "vpc_id" {
+  value       = module.vpc.vpc_id
+  description = "The VPC id"
+}
+
+output "vpc_cidr_block" {
+  value       = local.local_data.environment.inputs.vpc_cidr
+  description = "The VPC id"
+}
+
+output "vpc_security_group_id" {
+  description = "The ID of the security group created by default on VPC creation"
+  value       = module.vpc.default_security_group_id
+}
