995076583199188ec178ff39c851f494d5bba4e8
sbx_user1051 <sbx_user1051@169.254.114.229>
2022-09-22 16:36:30 +0000

    Update requested for Proton - environment/vpc-demo

---
+++ vpc-demo/outputs.tf
@@ -0,0 +1,23 @@
+/*
+This file is no longer managed by AWS Proton. The associated resource has been deleted in Proton.
+*/
+
+output "vpc_id" {
+  value       = module.vpc.vpc_id
+  description = "The VPC id"
+}
+
+output "vpc_cidr_block" {
+  value       = local.local_data.environment.inputs.vpc_cidr
+  description = "The VPC id"
+}
+
+output "vpc_security_group_id" {
+  description = "The ID of the security group created by default on VPC creation"
+  value       = module.vpc.default_security_group_id
+}
+
+output "aws_region" {
+  description = "The VPC aws region"
+  value       = local.local_data.environment.inputs.aws_region
+}
